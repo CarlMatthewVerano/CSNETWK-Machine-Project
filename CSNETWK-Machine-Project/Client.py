6fa4bd40594ca1e26fe3d40999439e5d6db0d982
Milkz03 <mikkel_gamboa@dlsu.edu.ph>
2023-11-21 22:59:10 +0800

    Update Client.py

--- CSNETWK-Machine-Project/Client.py
+++ CSNETWK-Machine-Project/Client.py
@@ -1,12 +1,49 @@
 import socket
 import os
 
-def main():
-    sServerAddress = input("Enter server address: ")
-    nPort = int(input("Enter port number: "))
+def userInput(userCommand):
+    print(userCommand)
+    specCommand = userCommand.split()
 
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientEndpoint:
+    if specCommand[0] == "/join":
+        print("User in")
+        try:
+            fileTransferConnection(specCommand[1], int(specCommand[2]))
+        except:
+            print("Error: Connection to the Server has failed! Please check IP Address and Port Number.")
+        finally:
+            print("Client: Connection is terminated.")
+
+    elif specCommand[0] == "/leave":
+        print("Error: Disconnection failed. Please connect to the server first.")
+
+    elif specCommand[0] == "/register":
+        print("Error: Registration failed. Please connect to the server first.")
+
+    elif specCommand[0] == "/store":
+        print("Error: Store failed. Please connect to the server first.")
+
+    elif specCommand[0] == "/dir":
+        print("Error: Request failed. Please connect to the server first.")
+
+    elif specCommand[0] == "/get":
+        print("Error: Fetch failed. Please connect to the server first.")
+
+    elif specCommand[0] == "/?":
+        print("[1] /join <server_ip_add> <port>\t - Connect to the server application\n")
+        print("[2] /leave\t\t\t\t - Disconnect from the server application\n")
+        print("[3] /register <handle>\t\t\t - Register a unique handle or alias\n")
+        print("[4] /store <filename>\t\t\t - Send file to server\n")
+        print("[5] /dir <server_ip_add> <port>\t\t - Request directory file list from a server\n")
+        print("[6] /get <filename> <port>\t\t - Fetch a file from a server\n")
+        print("[7] /?\t\t\t\t\t - Request command help to output all Input Syntax commands for references\n")
+
+    else:
+        print("Error: Command not found.")
+
+def fileTransferConnection(sServerAddress, nPort):
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientEndpoint:
             clientEndpoint.connect((sServerAddress, nPort))
             print("Client: Connected to server at", clientEndpoint.getpeername())
 
@@ -30,10 +67,9 @@ def main():
 
             print("Client: Downloaded file Received.txt")
 
-    except Exception as e:
-        print(e)
-    finally:
-        print("Client: Connection is terminated.")
+def main():
+    userCommand = input("Enter command: ")
+    userInput(userCommand)
 
 if __name__ == "__main__":
     main()
